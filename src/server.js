307b68fbd7569acb62ebd4f7bf929a72ac909d20
LWRGitHub <LoganReynoldsLWR@gmail.com>
2021-02-01 23:26:02 -0800

    chages on main index posts-new README server.js

---
+++ src/server.js
@@ -0,0 +1,28 @@
+require('dotenv/config')
+const path = require("path")
+const express = require('express');
+const exphbs  = require('express-handlebars');
+
+const app = express();
+
+app.engine('handlebars', exphbs({
+    layoutsDir: __dirname + '/views/layouts',
+    defaultLayout: 'main'
+}));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'handlebars');
+
+app.get("/", function(req,res){
+    return res.render("index")
+})
+
+app.get("/posts/new", function(req,res){
+    return res.render("posts-new")
+})
+
+
+// Start Server
+app.listen(process.env.PORT, () => {
+    console.log(`Example app listening on port ${process.env.PORT}!`)
+})
+  
